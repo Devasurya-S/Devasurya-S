de711a58b1752257c96a6b632c84dbfac2904de1
Devasurya-S <85778159+Devasurya-S@users.noreply.github.com>
2023-02-02 11:57:58 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
 //This function gets the html filename and modifies the href of css using the extracted name as the window size changes.
 
+// function layoutHandler() {
+
+// 	var styleLinkOne = document.getElementById("pageStyleOne");
+//     var styleLinkTwo = document.getElementById("pageStyleTwo");
+// 	var url = window.location.pathname;
+// 	var filename = url.substring(url.lastIndexOf('/')+1);
+// 	var name = filename.split(".");
+// 	var mobileStyleOne = "styles/Mobile/"+name[0]+".css";
+// 	var mobileStyleTwo = "styles/Mobile/style.css";
+// 	var desktopStyleOne = "styles/Desktop/"+name[0]+".css";
+// 	var desktopStyleTwo = "styles/Desktop/style.css";
+
+
+// 	if(window.innerWidth < 800){
+
+// 		styleLinkOne.setAttribute("href", mobileStyleOne);
+//         styleLinkTwo.setAttribute("href", mobileStyleTwo);
+
+// 	}   else {	
+
+//             styleLinkOne.setAttribute("href", desktopStyleOne);
+//             styleLinkTwo.setAttribute("href", desktopStyleTwo);
+
+// 	}
+// }
+
+// var value = document.getElementById("fileName");
+
+//var value = document.currentScript.getAttribute('one');
+var value = document.querySelector('#fileName');
+var fileName = value.getAttribute('target');
+
 function layoutHandler() {
 
 	var styleLinkOne = document.getElementById("pageStyleOne");
     var styleLinkTwo = document.getElementById("pageStyleTwo");
-	var url = window.location.pathname;
-	var filename = url.substring(url.lastIndexOf('/')+1);
-	var name = filename.split(".");
-	var mobileStyleOne = "styles/Mobile/"+name[0]+".css";
+	var mobileStyleOne = "styles/Mobile/" + fileName + ".css";
 	var mobileStyleTwo = "styles/Mobile/style.css";
-	var desktopStyleOne = "styles/Desktop/"+name[0]+".css";
+	var desktopStyleOne = "styles/Desktop/" + fileName + ".css";
 	var desktopStyleTwo = "styles/Desktop/style.css";
-
 
 	if(window.innerWidth < 800){
 
@@ -28,5 +56,4 @@ function layoutHandler() {
 
 window.onresize = layoutHandler;
 layoutHandler();
-
 //End
